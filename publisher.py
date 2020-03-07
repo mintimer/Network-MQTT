95a711320837507aab81b30de1cbe3943b913be0
mintimer <mintmafiacake@gmail.com>
2020-03-07 13:56:52 +0700

    debug 0.1

--- publisher.py
+++ publisher.py
@@ -4,19 +4,25 @@ def main():
     # Connect the socket to the port where the server is listening
     server_address = (sys.argv[1], 20000)
     print('connecting to %s port %s' % server_address)
-    sock.connect(server_address)
-    topic = sys.argv[2]
-    message = sys.argv[3]
-
     try:
-        # Send data
-        print('sending ' + message + ' to ' + topic)
-        message = 'PUB\t' + topic + '\t' + message
-        sock.send(message.encode())
+        sock.connect(server_address)
+        topic = sys.argv[2]
+        message = sys.argv[3]
+        try:
+            # Send data
+            print('sending ' + message + ' to ' + topic)
+            message = 'PUB\t' + topic + '\t' + message
+            sock.send(message.encode())
 
-    finally:
-        print('closing socket')
-        sock.close()
+        except BlockingIOError: 
+            pass
+        
+    except TimeoutError:
+        print('Connection Error')
+        try:
+            sys.exit(0)
+        except:
+            os._exit(0)
 
 if __name__ == '__main__':
     try:
@@ -24,6 +30,7 @@ if __name__ == '__main__':
         main()
     except KeyboardInterrupt:
         try:
+            print('closing socket')
             sock.close()
             sys.exit(0)
         except:
